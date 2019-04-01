ef0926f37f61d186f9ab8adb099ced95dea5e196
LaRaye Johnson <lnj2105@gmail.com>
2019-04-01 16:37:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,7 @@ class Student
     sql = "SELECT * FROM students WHERE name = ? LIMIT 1;"
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
+      binding.pry
     end.first
   end 
   
